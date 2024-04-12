922614db263b75f71bc67a26ac6bd1a7d17c0f5b
Brandon Miller <brandon@vector35.com>
2024-04-12 14:35:02 -0400

    Setting EFI module entry function for module type
    TE's use PEI module entry point type and PE's use DXE module entry
    point type

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from binaryninja import PluginCommand, BinaryView, BackgroundTaskThread, log_alert
+from .moduletype import identify_efi_module_type, set_efi_module_entry_type, EFIModuleType
 from .protocols import (
     init_protocol_mapping,
     define_handle_protocol_types,
@@ -20,34 +21,26 @@ def resolve_efi(bv: BinaryView):
             super().__init__("Initializing EFI protocol mappings...", True)
             self.bv = bv
 
-        def run(self):
-            if not init_protocol_mapping():
+        def _resolve_dxe(self):
+            self.progress = "Propagating EFI system table pointers..."
+            if not propagate_system_table_pointers(self.bv, self):
                 return
 
-            if "EFI_SYSTEM_TABLE" not in self.bv.types:
-                log_alert("This binary is not using the EFI platform. Use Open with Options when loading the binary to select the EFI platform.")
+            self.progress = "Defining types for uses of HandleProtocol..."
+            if not define_handle_protocol_types(self.bv, self):
                 return
 
-            self.bv.begin_undo_actions()
-            try:
-                self.progress = "Propagating EFI system table pointers..."
-                if not propagate_system_table_pointers(self.bv, self):
-                    return
+            self.progress = "Defining types for uses of OpenProtocol..."
+            if not define_open_protocol_types(self.bv, self):
+                return
 
-                self.progress = "Defining types for uses of HandleProtocol..."
-                if not define_handle_protocol_types(self.bv, self):
-                    return
+            self.progress = "Defining types for uses of LocateProtocol..."
+            if not define_locate_protocol_types(self.bv, self):
+                return
 
-                self.progress = "Defining types for uses of OpenProtocol..."
-                if not define_open_protocol_types(self.bv, self):
-                    return
-
-                self.progress = "Defining types for uses of LocateProtocol..."
-                if not define_locate_protocol_types(self.bv, self):
-                    return
-
-                # SMM/MM types cannot be propagated until EFI_BOOT_SERVICES types are propagated and the
-                # EFI_MM_BASE_PROTOCOL or EFI_SMM_BASE2_PROTOCOL is resolved
+            # SMM/MM types cannot be propagated until EFI_BOOT_SERVICES types are propagated and the
+            # EFI_MM_BASE_PROTOCOL or EFI_SMM_BASE2_PROTOCOL is resolved
+            if "EFI_MM_SYSTEM_TABLE" in self.bv.types:
                 self.progress = "Defining types for SMM/MM system tables..."
                 if not define_locate_mm_system_table_types(self.bv, self) or not define_locate_smm_system_table_types(
                     self.bv, self
@@ -65,6 +58,32 @@ def resolve_efi(bv: BinaryView):
                     self.bv, self
                 ):
                     return
+
+        def run(self):
+            if not init_protocol_mapping():
+                return
+
+            if "EFI_SYSTEM_TABLE" not in self.bv.types:
+                log_alert("This binary is not using the EFI platform. Use Open with Options when loading the binary to select the EFI platform.")
+                return
+
+            module_type = identify_efi_module_type(self.bv)
+            if module_type == EFIModuleType.UNKNOWN:
+                log_alert("Could not identify the EFI module type.")
+                return
+
+            self.bv.begin_undo_actions()
+            try:
+                try:
+                    set_efi_module_entry_type(self.bv, module_type)
+                except SyntaxError:
+                    log_alert("Failed to set entry function type. Update Binary Ninja for latest EFI type definitions.")
+                    return
+
+                if module_type == EFIModuleType.DXE:
+                    self._resolve_dxe()
+                elif module_type == EFIModuleType.PEI:
+                    log_alert("TE PEI modules are not yet supported.")
             finally:
                 self.bv.commit_undo_actions()
 
